e6dc8f6150b101b5786b9f4eb9f1ae98ccb1b097
Rita-Centeno <20211579@novaims.unl.pt>
2024-11-10 20:01:23 +0000

    correction on plot hist from utils1 + imputation processes

--- utils1.py
+++ utils1.py
@@ -129,7 +129,7 @@ def plot_histogram(ax, data, variable, x_label, y_label='Count', color='cadetblu
     Returns:
         None
     """
-    plt.hist(data[variable], bins=50, color=color)  # Plot the histogram using 50 bins
+    ax.hist(data[variable], bins=50, color=color)  # Plot the histogram using 50 bins
 
     set_plot_properties(ax, x_label, y_label)  # Set plot properties using helper function
 
